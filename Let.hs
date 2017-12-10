2fb65295d99e8de066dcadd0a8577741654716b2
chessai <chessai1996@gmail.com>
2017-12-10 17:55:26 -0500

    rename Where -> Let

--- Let.hs
+++ Let.hs
@@ -3,12 +3,12 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
-type family Where a where
-  Where a = a
+type family Let a where
+  Let a = a
 
 type family a =: b where
   a =: b = a ~ b
 
-lmao :: Where d =: Double => d -> d -> d
+lmao :: Let d =: Double => d -> d -> d
 lmao = (+)
 
