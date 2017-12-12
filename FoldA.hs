8f0e0c1a469a71ad18ac5977763645be77114a6c
chessai <chessai1996@gmail.com>
2017-12-12 15:39:07 -0500

    foldmapA

---
+++ FoldA.hs
@@ -0,0 +1,18 @@
+-- credit: David Feuer <david.feuer@gmail.com>
+module FoldA
+  ( Ap(..)
+  , foldMapA
+  ) where
+
+import Control.Applicative
+import Data.Foldable
+import Data.Monoid
+
+newtype Ap f a = Ap { getAp :: f a }
+
+instance (Applicative f, Monoid a) => Monoid (Ap f a) where
+  mempty = Ap $ pure mempty
+  mappend (Ap x) (Ap y) = Ap $ liftA2 mappend x y
+
+foldMapA :: (Foldable t, Monoid m, Applicative f) => (a -> f m) -> t a -> f m
+foldMapA f = getAp . foldMap (Ap . f)
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # useless
 
 random haskell thoughts, either mine or collected from elsewhere.
-i don't think any of these are good ideas.
